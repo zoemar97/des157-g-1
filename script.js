39e3c69fbe45d3af78bc87f268ef7eee623973be
Alexandra Martinez <alexmar@scc2102-15.ucdavis.edu>
2019-01-14 11:45:31 -0800

    init

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log('reading js')
+function setup () {
+  //size is createCanvas in p5
+  //create a variable to reference the Canvas
+var myCanvas = createCanvas(800,250);
+
+  //connect myCanvas to mySketch
+  myCanvas.parent('mySketch');
+}
+
+  function draw () {
+    ellipse(mouseX,mouseY,50,50);
+ }
+
+  //var y= 1;
+  //var x= 2;
+
+  //function setup(){
+    //size is createCanvas in p5
+    //create a variable to reference the Canvas
+    //var myCanvas = createCanvas(800,250);
+    //background(#16AFCB);
+
+    //connect myCanvas to mySketch
+    //myCanvas.parent('mySketch');
+
+  //}
+    //function draw(){
+
+      //for (var x =0; x<=width; x+=40)
+      //  {
+
+      //for (var y=0; y<=height; y+=40)
+      //  {
+
+       //ellipse(x, y/2, 15, 15);
+       //fill(#FEEF63);
+
+        //}
+
+    //  if ((mouseY>=-50) && (mouseY<=400) && (mouseX>=0) && (mouseX<=450)) {
+      //  stroke(random(450));
+
+      // }}
+
+//  }
