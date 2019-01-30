c8af804a1daf7c1bc9d5d6aaecfef6d69b7fda9e
alexandra <aleeeeeexx_@Alexandras-MacBook-Pro-2.local>
2019-01-29 23:25:32 -0800

    js changes

--- studio1/script.js
+++ studio1/script.js
@@ -1,37 +1,47 @@
 'use strict';
 console.log('reading js');
 
-document.querySelector('form').addEventListener('submit',processForm);
-document.querySelector('#reset').addEventListener('reset',resetform);
+document.querySelector('form').addEventListener('submit', processForm);
+document.querySelector('#r').addEventListener('click', resetForm);
 
 //define process function
-function processForm(){
+function processForm(evt) {
 
-var -ingverb = document.getElementbyID('#-ingverb').value;
-var noun1 = document.getElementbyID('#noun1').value;
-var adjective1 = document.getElementbyID('#adjective1').value;
-var noun2 = document.getElementbyID('#noun2').value;
-var adjective2 = document.getElementbyID('#adjective2').value;
-var verb = document.getElementbyID('#verb').value;
-var name = document.getElementbyID('#name').value;
-var gender = document.getElementbyID('#gender').value;
-var noun3 = document.getElementbyID('#noun3').value;
+  var -ingverb = document.querySelector('#-ingverb').value;
+  var noun1 = document.querySelector('#noun1').value;
+  var adjective1 = document.querySelector('#adjective1').value;
+  var noun2 = document.querySelector('#noun2').value;
+  var adjective2 = document.querySelector('#adjective2').value;
+  var verb = document.querySelector('#verb').value;
+  var name = document.querySelector('#name').value;
+  var gender = document.querySelector('#gender').value;
+  var noun3 = document.querySelector('#noun3').value;
 
-var myMsg = document.querySelector('#myMsg');
-var msgSection = document.querySelector('#msgSection');
+  var myMsg = document.querySelector('#myMsg');
+  var msgSection = document.querySelector('#msgSection');
 
-//concatenating a message using .innerHTML()
-myMsg.innerHTML=
-  'One Valentines day, I was '+ userName +' when I looked in my '+ noun1
-  'and saw a' -ingverb + major '!'
-  'It said will you be my ' + noun3 + 'valentine?'
-  'I ' noun4 + ' to see who it was from. ' + ' Turns out it was from '+
-  noun5 + 'I am such a lucky' + noun6 + 'to have ' + noun7 'in my life!';
+  //concatenating a message using .innerHTML()
+  myMsg.innerHTML =
+    'One Valentines day,<p> I was' +
+    -ingverb +
+    'when I looked in my' +
+    noun1 'and saw a' +
+    adjective1 + noun2 +
+    '!' + 'It said will you be my ' +
+    adjective2 +
+    'valentine?' + 'I ' +
+    verb +
+    ' to see who it was from. ' + ' Turns out it was from ' +
+    name +
+    'I am such a lucky' +
+    gender +
+    'to have ' +
+    noun3 + 'in my life!</p>';
 
-msgSection.class= 'show';
+  msgSection.className = 'show';
 
-evt.preventfault()
-  return false;//stops default behavior
+  evt.preventfault();
+  return false; //stops default behavior
 }
 
 function resetform() {
