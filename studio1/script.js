bdc8cd1cd235b5302901ee16e3d4c51009c13858
Alexandra Martinez <alexmar@scc2101m-11.ucdavis.edu>
2019-01-29 18:17:52 -0800

    js

---
+++ studio1/script.js
@@ -0,0 +1,22 @@
+'use strict';
+console.log('reading js');
+
+var userName = document.getElementbyID('userName').value;
+var userName = document.getElementbyID('#username').value;
+
+var noun = document.getElementbyID('noun').value;
+var noun = document.getElementbyID('#noun').value;
+
+var userName = document.getElementbyID('userName').value;
+var userName = document.getElementbyID('#username').value;
+
+var userName = document.getElementbyID('userName').value;
+var userName = document.getElementbyID('#username').value;
+
+submit.addEventListener('click',processForm);
+
+function processForm(){
+
+  //concatenate a message using .innerHTML()
+  return false;//stops default behavior
+}
